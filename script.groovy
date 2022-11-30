48a067db9567988cdaca28d5d90421e99ca4acd3
Sure-kha <68910436+Sure-kha@users.noreply.github.com>
2022-11-30 18:41:30 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,4 +1,11 @@
 def buildApp(){
   echo 'building the application...'
 }
+
+def testApp(){
+  echo 'test the application...'
+}
+def deployApp(){
+  echo 'deploy the application...'
+}
 return this
